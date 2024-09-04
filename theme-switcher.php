815c39e0f56bd01007b11851e72429d8c5a1ea13
MetaControl <81246931+MetaControl@users.noreply.github.com>
2024-09-04 18:32:21 +0200

    Initial theme-domain-switcher.php

--- theme-switcher.php
+++ theme-switcher.php
@@ -1,43 +1,49 @@
 <?php
 
 /**
- * Plugin Name: Theme Switcher
- * Description: This plugin allows you to switch between themes and child themes with dynamic site identity and icon switching.
+ * Plugin Name: Theme and Domain Switcher
+ * Description: A plugin to switch between themes and child themes based on domains with dynamic site identity and icon switching.
  * Author: WeaveMaster
- * Author URI: https://teehautecouture.com
- * Version: 1.5
+ * Author URI: https://example.com
+ * Version: 2.0
  */
-/**
- * ======== Customizable Variables Section ========
- * You can modify these variables to set your theme slugs, names, site titles, taglines, and favicon URLs.
- * the favicons here are set up to be PNGs, but it should work with an .ico too.
- */
+
+// ======== Customizable Variables Section ========
 
 // Allowed roles for theme switching
 $theme_switcher_allowed_roles = array('administrator', 'editor'); // Add more roles as needed
 
-// Theme data for switching and site identity
-$themes_identity = array(
-    'theme_slug_1' => array(  // Replace 'theme_slug_1' with the actual slug of the first theme
-        'name'            => 'Theme 1 Name', // The name of the first theme
-        'blogname'        => 'Site Title for Theme 1', // The site title to be used for this theme
-        'blogdescription' => 'Site Tagline for Theme 1', // The site tagline to be used for this theme
-        'favicon_url'     => content_url('/path/to/favicon-1.png'), // Path to the favicon for this theme
-        'icon_url'        => '/path/to/icon-1.png' // Icon URL for the admin bar or any icon use for this theme
+// Fallback or Base Domain (Core Domain) - used if no specific domain matches
+$fallback_domain = array(
+    'url'            => 'https://fallback-domain.com',  // Replace with the actual fallback domain URL
+    'theme_slug'     => 'fallback_theme_slug',  // Replace with the slug of the fallback theme
+    'blogname'       => 'Fallback Site Title',  // Replace with the site title for the fallback domain
+    'blogdescription'=> 'Fallback Site Tagline',  // Replace with the site tagline for the fallback domain
+    'favicon_url'    => content_url('/path/to/favicon-fallback.png'),  // Path to favicon for the fallback domain
+    'icon_url'       => '/path/to/icon-fallback.png'  // Icon URL for the fallback domain
+);
+
+// Domain data for switching and site identity
+$domains_identity = array(
+    'domain_1' => array(  // Domain 1
+        'url'            => 'https://domain1.com',  // Replace with the actual domain URL
+        'theme_slug'     => 'theme_slug_1',  // Replace with the actual slug of the theme for this domain
+        'blogname'       => 'Site Title 1',  // Replace with the site title for this domain
+        'blogdescription'=> 'Site Tagline 1',  // Replace with the site tagline for this domain
+        'favicon_url'    => content_url('/path/to/favicon1.png'),  // Path to favicon for this domain
+        'icon_url'       => '/path/to/icon1.png'  // Icon URL for this domain
     ),
-    'theme_slug_2' => array(  // Replace 'theme_slug_2' with the actual slug of the second theme
-        'name'            => 'Theme 2 Name', // The name of the second theme
-        'blogname'        => 'Site Title for Theme 2', // The site title to be used for this theme
-        'blogdescription' => 'Site Tagline for Theme 2', // The site tagline to be used for this theme
-        'favicon_url'     => content_url('/path/to/favicon-2.png'), // Path to the favicon for this theme
-        'icon_url'        => '/path/to/icon-2.png' // Icon URL for the admin bar or any icon use for this theme
+    'domain_2' => array(  // Domain 2
+        'url'            => 'https://domain2.com',  // Replace with the actual domain URL
+        'theme_slug'     => 'theme_slug_2',  // Replace with the actual slug of the theme for this domain
+        'blogname'       => 'Site Title 2',  // Replace with the site title for this domain
+        'blogdescription'=> 'Site Tagline 2',  // Replace with the site tagline for this domain
+        'favicon_url'    => content_url('/path/to/favicon2.png'),  // Path to favicon for this domain
+        'icon_url'       => '/path/to/icon2.png'  // Icon URL for this domain
     )
 );
 
-/**
- * ======== End of Customizable Variables Section ========
- */
-
+// ======== End of Customizable Variables Section ========
 
 /**
  * Function to get allowed user roles that can see and use the theme switcher
@@ -61,6 +67,48 @@ function user_has_allowed_role() {
 }
 
 /**
+ * Function to get the current domain settings
+ */
+function get_current_domain_settings() {
+    global $domains_identity, $fallback_domain;
+    $current_domain = home_url();  // Get current site URL
+
+    // Loop through each domain in the identity list
+    foreach ($domains_identity as $domain_key => $domain_data) {
+        if (strpos($current_domain, $domain_data['url']) !== false) {
+            return $domain_data;
+        }
+    }
+
+    // Return fallback domain if no match found
+    return $fallback_domain;
+}
+
+/**
+ * Function to dynamically change the site title, tagline, and favicon based on the current domain
+ */
+function dynamic_domain_identity() {
+    $domain_settings = get_current_domain_settings();
+    
+    // Update site title, tagline, and favicon based on the current domain
+    add_filter('option_blogname', function() use ($domain_settings) {
+        return $domain_settings['blogname'];
+    });
+    
+    add_filter('option_blogdescription', function() use ($domain_settings) {
+        return $domain_settings['blogdescription'];
+    });
+    
+    add_filter('get_site_icon_url', function() use ($domain_settings) {
+        return $domain_settings['favicon_url'];
+    });
+
+    // Switch to the appropriate theme based on the domain
+    switch_theme($domain_settings['theme_slug']);
+}
+add_action('init', 'dynamic_domain_identity');
+
+/**
  * Function to generate the theme switch URL with nonce
  */
 function get_theme_switch_url($theme_slug) {
@@ -71,32 +119,31 @@ function get_theme_switch_url($theme_slug) {
 }
 
 /**
- * Function to add the theme switcher to the admin bar
+ * Function to add theme switcher to admin bar (optional, shown for allowed roles)
  */
 function add_theme_switcher_to_admin_bar($wp_admin_bar) {
     if (!user_has_allowed_role()) {
         return;
     }
 
-    $current_theme = wp_get_theme();
-    $themes = wp_get_themes();
-    global $themes_identity;
+    $current_domain = get_current_domain_settings();
+    global $domains_identity;
 
     $wp_admin_bar->add_menu(array(
-        'id'    => 'theme_switcher_parent',
-        'title' => 'Switch Theme',
+        'id'    => 'domain_switcher_parent',
+        'title' => 'Switch Site',
         'href'  => '#',
     ));
 
-    foreach ($themes_identity as $theme_slug => $theme_data) {
-        if ($current_theme->get('Name') === $theme_data['name']) {
-            continue; // Skip active theme
+    foreach ($domains_identity as $domain_key => $domain_data) {
+        if ($current_domain['url'] === $domain_data['url']) {
+            continue;  // Skip the current domain
         }
         $wp_admin_bar->add_menu(array(
-            'parent' => 'theme_switcher_parent',
-            'id'     => 'theme_switcher_' . $theme_slug,
-            'title'  => $theme_data['name'],
-            'href'   => get_theme_switch_url($theme_slug),
+            'parent' => 'domain_switcher_parent',
+            'id'     => 'domain_switcher_' . $domain_key,
+            'title'  => esc_html($domain_data['blogname']),
+            'href'   => esc_url($domain_data['url']),
         ));
     }
 }
@@ -119,15 +166,15 @@ function theme_switcher_shortcode() {
     }
 
     $current_theme = wp_get_theme();
-    global $themes_identity;
+    global $domains_identity;
     $output = '<ul class="theme-switcher">';
 
-    foreach ($themes_identity as $theme_slug => $theme_data) {
-        if ($current_theme->get('Name') === $theme_data['name']) {
+    foreach ($domains_identity as $theme_slug => $theme_data) {
+        if ($current_theme->get('Name') === $theme_data['blogname']) {
             continue; // Skip active theme
         }
         $output .= '<li><a href="' . esc_url(get_theme_switch_url($theme_slug)) . '">'
-            . esc_html($theme_data['name']) . '</a></li>';
+            . esc_html($theme_data['blogname']) . '</a></li>';
     }
 
     $output .= '</ul>';
@@ -145,13 +192,13 @@ function simple_theme_switcher_icon() {
     }
 
     $current_theme = wp_get_theme();
-    global $themes_identity;
+    global $domains_identity;
 
     // Determine which theme is active and set the icon and theme switch URL
-    foreach ($themes_identity as $theme_slug => $theme_data) {
-        if ($current_theme->get('Name') === $theme_data['name']) {
-            $next_theme_slug = array_keys($themes_identity)[1 - array_search($theme_slug, array_keys($themes_identity))]; // Switch to the other theme
-            $icon_url = $themes_identity[$theme_slug]['icon_url'];
+    foreach ($domains_identity as $theme_slug => $theme_data) {
+        if ($current_theme->get('Name') === $theme_data['blogname']) {
+            $next_theme_slug = array_keys($domains_identity)[1 - array_search($theme_slug, array_keys($domains_identity))]; // Switch to the other theme
+            $icon_url = $domains_identity[$theme_slug]['icon_url'];
             break;
         }
     }
@@ -163,30 +210,3 @@ function simple_theme_switcher_icon() {
             </a>';
 }
 add_shortcode('theme_switch_icon', 'simple_theme_switcher_icon');
-
-/**
- * Function to dynamically change the site title, tagline, and favicon based on the active theme
- */
-function dynamic_site_identity() {
-    global $themes_identity;
-    $current_theme = wp_get_theme();
-
-    // Loop through the theme identity array to find the current theme
-    foreach ($themes_identity as $slug => $theme_data) {
-        if ($current_theme->get('Name') === $theme_data['name']) {
-            // Update title and tagline
-            add_filter('option_blogname', function() use ($theme_data) { return $theme_data['blogname']; });
-            add_filter('option_blogdescription', function() use ($theme_data) { return $theme_data['blogdescription']; });
-            
-            // Override the favicon with the correct one for the active theme
-            add_filter('get_site_icon_url', function() use ($theme_data) {
-                return $theme_data['favicon_url'];
-            });
-        }
-    }
-}
-add_action('init', 'dynamic_site_identity');
-
-
-}
-add_action('wp_head', 'dynamic_site_identity');
